6e5350ba6726dd408113c7e10a52191a55188723
Cliff Hall <cliff@futurescale.com>
2012-08-26 21:00:57 -0400

    adding .gitignore

---
+++ src/controller/command/PrepModelCommand.js
@@ -0,0 +1,29 @@
+/**
+ * @author Mike Britton
+ * 
+ * @class ModelPrepCommand
+ * @link https://github.com/mbritton/todopuremvc.git
+ * 
+ * 
+ */
+
+puremvc.define
+(
+    // CLASS INFO
+    {
+        name:'todomvc.controller.command.PrepModelCommand',
+        parent:puremvc.SimpleCommand
+    },
+  
+    // INSTANCE MEMBERS
+    {
+        /** @override */
+        execute: function (note)
+        {
+            this.facade.registerProxy( new todomvc.model.proxy.TodoProxy );
+            
+            this.sendNotification(todomvc.AppConstants.LOAD_TODOS);
+            this.sendNotification(todomvc.AppConstants.COMPUTE_STATS);
+        }
+    }    
+);
